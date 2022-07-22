b756b308ddc7adb7aa0e41e6bc1600181f8e43c0
lihaiyu <2241412234@qq.com>
2022-07-22 15:32:25 +0800

    uodate start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,3 @@
 #java -cp "jenkins-demo-1.0-SNAPSHOT.jar;lib/*" cn.lihaiyu.demo.SpringBootApplicationRun
 source /etc/profile
-java -cp "../lib/*" cn.lihaiyu.demo.SpringBootApplicationRun
+java -cp "../lib/*" cn.lihaiyu.demo.SpringBootApplicationRun &
