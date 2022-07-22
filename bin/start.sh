08a1a3b6d04a9dc56e5a9aa0b2490eb8960d872a
lihaiyu <2241412234@qq.com>
2022-07-22 13:14:15 +0800

    update

--- bin/start.sh
+++ bin/start.sh
@@ -1,1 +1,2 @@
-java -cp "jenkins-demo-1.0-SNAPSHOT.jar;lib/*" cn.lihaiyu.demo.SpringBootApplicationRun
+#java -cp "jenkins-demo-1.0-SNAPSHOT.jar;lib/*" cn.lihaiyu.demo.SpringBootApplicationRun
+java -cp "lib/*" cn.lihaiyu.demo.SpringBootApplicationRun
